5199e3b0b17c86421b67e107f9d77d5b8ac4236d
KosmachMariia <kosmach_2004@mail.ru>
2025-05-23 20:48:19 +0300

    Сделан профиль и экран расписания

--- app/src/main/java/com/example/crmedumobile/presentation/MainActivity.kt
+++ app/src/main/java/com/example/crmedumobile/presentation/MainActivity.kt
@@ -6,9 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.crmedumobile.presentation.navigation.RootNavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.crmedumobile.presentation.screen.ProfileScreen
+import com.example.crmedumobile.presentation.screen.ScheduleScreen
 import com.example.crmedumobile.presentation.theme.CrmedumobileTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,8 +27,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CrmedumobileTheme {
-                RootNavGraph()
+                //RootNavGraph()
+                //ProfileScreen()
+                MainScreen()
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    var currentScreen by remember { mutableStateOf(com.example.crmedumobile.presentation.states.Screen.NOTES) }
+
+    NavHost(
+        navController = navController,
+        startDestination = com.example.crmedumobile.presentation.states.Screen.NOTES.name
+    ) {
+        composable(com.example.crmedumobile.presentation.states.Screen.NOTES.name) {
+            ScheduleScreen(
+                onNavigate = { screen ->
+                    currentScreen = screen
+                    navController.navigate(screen.name) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = false
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+        composable(com.example.crmedumobile.presentation.states.Screen.PROFILE.name) {
+            ProfileScreen(
+                onNavigate = { screen ->
+                    currentScreen = screen
+                    navController.navigate(screen.name) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = false
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+        composable(com.example.crmedumobile.presentation.states.Screen.NOTIFICATIONS.name) {
+            Text("Notifications Screen")
+        }
+        composable(com.example.crmedumobile.presentation.states.Screen.CALENDAR.name) {
+            Text("Calendar Screen")
         }
     }
 }
